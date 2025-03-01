0260a9e85a00ec3b8e685b822fa887e843a3c8c6
CrisR <104317565+CrisRaptor@users.noreply.github.com>
2025-03-01 18:53:29 +0100

    Rutas establecidas e Inicio

--- fruteria/src/app/app.routes.ts
+++ fruteria/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './shared/inicio/inicio.component';
+import { GestorTiendaComponent } from './shared/gestor-tienda/gestor-tienda.component';
+import { ProductoFormModificarComponent } from './features/producto/components/producto-form-modificar/producto-form-modificar.component';
+import { ProductoFormCrearComponent } from './features/producto/components/producto-form-crear/producto-form-crear.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: InicioComponent,
+        title: 'Tiendas'
+    },
+    {
+        path: 'tiendas/:id',
+        component: GestorTiendaComponent,
+        title: 'Tienda'
+    },
+    {
+        path: 'modificar/:id',
+        component: ProductoFormModificarComponent,
+        title: 'Modificar producto'
+    },
+    {
+        path: 'crear',
+        component: ProductoFormCrearComponent,
+        title: 'Crear producto'
+    }
+];
